8dd2f6763532989cb43947ed3e6b01145d73dfa8
sonu kushwaha <inkushwahasonu@gmail.com>
2020-08-11 12:39:02 +0530

    second example of type conversion in c++

--- cpp/introduction/type_conversion/01.cpp
+++ cpp/introduction/type_conversion/01.cpp
@@ -1,3 +1,6 @@
+// int to double
+
+
 #include<iostream>
 using namespace std;
 int main()
---
+++ cpp/introduction/type_conversion/02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int int_value;
+  double double_value=45.345;
+  int_value = double_value;
+  cout << "int value : " << int_value << endl;
+  cout << " Double value : " << double_value << endl ;
+  return 0 ;
+
+
+}
+
