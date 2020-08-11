3195a5e9e327bdea9209d58fca0241dcf5a01cf3
sonu kushwaha <inkushwahasonu@gmail.com>
2020-08-11 13:19:46 +0530

    third example of type conversion in c++

---
+++ cpp/introduction/type_conversion/03.cpp
@@ -0,0 +1,19 @@
+// type casting
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  double  double_value = 3.67 ;
+  cout << " double value : " << double_value << endl ;
+  int int_value1 = (int)double_value ;
+  cout << " int value 1 : " << int_value1 << endl ;
+  int int_value2 = int(double_value);
+  cout << " int value 2 : " << int_value2 << endl ;
+  //cout << int(double_value) << endl ;
+  //cout  << (int)double_value << endl ;
+  //cout << double_value << endl ;
+
+
+  return 0;
+}
