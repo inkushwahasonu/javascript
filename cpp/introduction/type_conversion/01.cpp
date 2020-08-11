72703c3348a655c0c7deaed96fa9a3acc7ef14f7
sonu kushwaha <inkushwahasonu@gmail.com>
2020-08-11 12:30:16 +0530

    first example of type conversion of c++

---
+++ cpp/introduction/type_conversion/01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num_int=20;
+  double num_double;
+  num_double=num_int;
+  cout<<"Integer Value : " << num_int <<endl;
+  cout<<"double Value : " << num_double <<endl ;
+  return 0;
+}
