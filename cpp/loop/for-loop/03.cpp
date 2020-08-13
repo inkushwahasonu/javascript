6b34298ceff1c9623a909fab7e1784ac0b1a1a42
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-13 18:56:30 +0530

    third example of for loop

--- cpp/loop/for-loop/03.cpp
+++ cpp/loop/for-loop/03.cpp
@@ -3,14 +3,15 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-  int num, sum ;
-  cout << " Entern a natural number : " << endl ;
-  cin >> num ;
-  for( int i=0; i<=num; ++i){
-    sum += i;
-  }
-  cout << " Sum of " << num << " natural number is : " << sum << endl ;
-  return 0;
+int main() {
+    int num, sum;
+    sum=0;
+    cout << " Entern a natural number : " << endl;
+    cin >> num;
+    for (int i=1; i<=num; ++i) {
+        sum += i;
+    }
+    cout << " Sum of " << num << " natural number is : " << sum << endl;
+    return 0;
 }
 
