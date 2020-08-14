dd699c6059db03256ec62ba71a34e1443727420d
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-14 18:51:46 +0530

    example of do-while loop of c++

---
+++ cpp/loop/while-loop/03.cpp
@@ -0,0 +1,13 @@
+ // do while loop 
+ // display numbrer from 1 to 5 
+ #include<iostream>
+using namespace std ;
+int main (){
+  int num=1; 
+  do{
+    cout << num << endl ;
+    ++num ;
+  }
+  while(num <= 5);
+  return 0;
+}
