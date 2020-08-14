f442e9f2c605feb305f6f3205eabfca7583fcc47
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-14 18:22:16 +0530

    final submition of second example of while loop in c++

--- cpp/loop/while-loop/02.cpp
+++ cpp/loop/while-loop/02.cpp
@@ -1,13 +1,18 @@
+// sum of only posutive numbers
+
+
+
 #include<iostream>
 using namespace std;
 int main (){
   int num, sum=0;
   cout << " enter a number " << endl ;
   cin >> num ;
-  while(int i >0){
+  while(num >=0){
     sum += num;
     cout << "Enter second number" << endl;
     cin >> num ;
   }
+  cout << " \nsum is : " << sum << endl ;
   return 0;
 }
