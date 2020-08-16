48d46467e8ed888936babf0221a55e8cc7ba8b56
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-16 16:02:00 +0530

    first example of function in c++

--- cpp/Flow-Control/goto/01.cpp
+++ cpp/Flow-Control/goto/01.cpp
@@ -1,22 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  float num, average, sum = 0.0 ;
+int main()
+{
+  float num, average, sum = 0.0;
   int i, n;
-  cout << " enter maximum number of input : " << endl ;
-  cin >> n ;
-  for (i=0; i<=n; ++i){
-    cout << " Enter n  " << i <<":"<<endl;
-    cin>> num ;
-    if(num<0.0){
-      goto  jump;
+  cout << " enter maximum number of input : " << endl;
+  cin >> n;
+  for (i = 0; i <= n; ++i)
+  {
+    cout << " Enter n  " << i << ":" << endl;
+    cin >> num;
+    if (num < 0.0)
+    {
+      goto jump;
     }
-    sum +=num;
-
+    sum += num;
   }
-  jump : 
-  average = sum/(i-1);
-  cout << " \n average : " << average <<endl;
-  return 0 ;
-
+jump:
+  average = sum / (i - 1);
+  cout << " \n average : " << average << endl;
+  return 0;
 }
---
+++ cpp/function/simple/01.cpp
@@ -0,0 +1,12 @@
+//simple function and function calling
+#include<iostream>
+using namespace std;
+void great(){
+  cout << "hello " << endl ;
+
+}
+int main(){
+  great();
+  return 0;
+
+}
