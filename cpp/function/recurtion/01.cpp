2fc423d53e7f0e0d8e4f3614ac2b4de99514e4a2
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-18 17:02:39 +0530

    recurtion in function inc++

---
+++ cpp/function/recurtion/01.cpp
@@ -0,0 +1,22 @@
+// factorial of number using recurtion
+
+#include<iostream>
+using namespace std;
+int factorial(int);
+int main(){
+  int n, result;
+  cout << "enter a non-negative number : ";
+  cin >> n;
+  result = factorial(n);
+  cout << "Factorial of "<< n << " = " << result;
+  return 0;
+}
+int factorial(int n ){
+  
+    if(n>1){
+      return n * factorial(n-1);
+    }else{
+      return 1;
+    }
+
+}
---
+++ cpp/function/storage-class/03.cpp
@@ -0,0 +1,14 @@
+// static local variable
+#include<iostream>
+using namespace std;
+void test(){
+  static int var = 0 ;
+  ++var;
+  cout << var << endl; 
+}
+int main()
+{
+  test();
+  test();
+  return 0;
+}
