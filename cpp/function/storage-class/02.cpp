c8d7407acd4d31422278f95d464241590c1dd6c7
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-18 08:56:30 +0530

    example of globbe variable of storage class in c++

---
+++ cpp/function/storage-class/02.cpp
@@ -0,0 +1,16 @@
+// global variable
+#include<iostream>
+using namespace std;
+int c = 12;
+void test();
+int main(){
+  ++c;
+  cout << c <<endl;
+  test();
+  return 0;
+}
+void test()
+{
+  ++c;
+  cout <<c;
+}
