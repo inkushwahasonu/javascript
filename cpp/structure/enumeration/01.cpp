18014e23e619642f1a37253abc947d0e7f20ebe1
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-19 18:37:21 +0530

    simple enumeration type of structure in c++

---
+++ cpp/structure/enumeration/01.cpp
@@ -0,0 +1,12 @@
+// enumeration type
+
+#include<iostream>
+using namespace std;
+
+enum week { Sunday , Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+int main(){
+  week today;
+  today = Wednesday;
+  cout << "Day "<<today+1 <<endl;
+  return 0;
+}
