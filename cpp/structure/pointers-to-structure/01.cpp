91362fa2ec88bbe6a7eab061edda00de2d133058
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-19 17:14:36 +0530

    pointers to structure in c++

---
+++ cpp/structure/pointers-to-structure/01.cpp
@@ -0,0 +1,20 @@
+//   pointers-to-structure
+
+#include<iostream>
+using namespace std;
+struct Distance{
+  int feet ; 
+  float inch;
+};
+int main()
+{
+  Distance *ptr, d;
+  ptr = &d;
+  cout <<"Enter feet : ";
+  cin >> (*ptr).feet;
+  cout << "enter inch :";
+  cin >> (*ptr).inch;
+  cout << "Displaying information."<<endl;
+  cout << "Distance = "<<(*ptr).feet<< " feet, " << (*ptr).inch<<" inches";
+  return 0;
+}
--- cpp/structure/structure-and-function/02.cpp
+++ cpp/structure/structure-and-function/02.cpp
@@ -1,25 +1,27 @@
 // returning structure from function in c++
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Person{
+struct Person
+{
   char name[50];
   int age;
   float salary;
-
 };
 
 Person getData(Person);
 void displayData(Person);
-int main(){
+int main()
+{
   Person p;
 
   p = getData(p);
   displayData(p);
   return 0;
 }
-Person getData(Person p){
+Person getData(Person p)
+{
   cout << "enter full name : ";
   cin.get(p.name, 50);
   cout << "enter age : ";
@@ -29,9 +31,10 @@ Person getData(Person p){
   return p;
 }
 
-void displayData(Person p){
-  cout << "\nDisplaying Information . "<< endl;
-  cout << "name : "<<p.name << endl;
-  cout<<"age : "<<p.age<<endl;
-  cout <<"salary : "<<p.salary;
+void displayData(Person p)
+{
+  cout << "\nDisplaying Information . " << endl;
+  cout << "name : " << p.name << endl;
+  cout << "age : " << p.age << endl;
+  cout << "salary : " << p.salary;
 }
