456b4199e1eefb08a96c51ad5dae12ffe07e9bdc
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-19 14:12:15 +0530

    string to read a work

---
+++ cpp/array-and-string/string/01.cpp
@@ -0,0 +1,14 @@
+// string to read a word
+
+#include<iostream>
+using namespace std;
+int main(){
+  char str[100];
+  cout << "enter a string : ";
+  cin >> str;
+  cout << "you entered : "<< str << endl;
+  cout << "\n Enter another string : ";
+  cin >> str;
+  cout << " you entered : " << str << endl;
+  return 0;
+}
