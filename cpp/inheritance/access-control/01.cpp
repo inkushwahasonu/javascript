79d35eafa97e470cb9ca7fc9582b7d2ce81c89ac
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-21 18:16:08 +0530

    Example of public, protected and private inheritance in C++

---
+++ cpp/inheritance/access-control/01.cpp
@@ -0,0 +1,32 @@
+//  example of public , protected and private in inheritance 
+
+
+{
+	public:
+		int x;
+	protected:
+		int y;
+	private:
+		int z;
+};
+
+class publicDerived: public base
+{
+	// x is public
+	// y is protected
+	// z is not accessible from publicDerived
+};
+
+class protectedDerived: protected base
+{
+	// x is protected
+	// y is protected
+	// z is not accessible from protectedDerived
+};
+
+class privateDerived: private base
+{
+	// x is private
+	// y is private
+	// z is not accessible from privateDerived
+}  
