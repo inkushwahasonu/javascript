5dbaddc84952e5662524c55898556ba75133cf53
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-21 12:44:02 +0530

    printing variable address

---
+++ cpp/pointer/simple/01.cpp
@@ -0,0 +1,15 @@
+// printing variable addresses in
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int var1 = 3;
+   int var2 = 24;
+   int var3 = 17;
+
+   cout << "address of var1 : "<< &var1 << endl;
+   cout << "address of var 2 : "<< &var2 << endl;
+   cout << "address of var3 : " << &var3 << endl;
+   return 0;
+}
