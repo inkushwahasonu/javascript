49f2f1a814676a5f2e444eb92f765bec7b7bc699
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-27 19:27:05 +0530

    parameter of function in javascript

---
+++ JavaScript/js-fundamentals/07-function-basic/parameters.js
@@ -0,0 +1,23 @@
+// parameters in function
+
+function showMessage(form, text){
+  console.log(form + ':' +text);
+}
+showMessage('Anna ', ' hello');       // Ann : hello
+showMessage('Anna ', " what's up");  // Anna : what's up
+
+
+
+//'''''''''''''''''''''''''''
+console.log(',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
+// ''''''''''''''''''''''''''
+ 
+
+function showDetail(form , text){
+  form = '*' + form + '*'; 
+  console.log(form + ' : ' + text);
+}
+let form = 'Anna';
+
+showDetail(form, "Hello");
+console.log(form);
