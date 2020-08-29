af601b9cbf241ed865e9cdec68bfd03b6b6eac96
inkushwahasonu <inkushwahasonu@gmail.com>
2020-08-29 19:32:17 +0530

    basic defrences in function declaration and function expration in javascript

---
+++ JavaScript/js-fundamentals/08-function-expration/expration-vs-declaration.js
@@ -0,0 +1,25 @@
+//function declaration vs function expration
+
+
+/* ----------*/console.log("function declaration");/*----------------*/
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(3, 4));
+
+
+/* ----------*/console.log("function expration");/*----------------*/
+
+let sub = function(a , b){
+  return a - b;
+}
+console.log(sub(9 , 3));
+
+
+
+
+
+/* ----------*/console.log("function declaration ");/*----------------*/
+/* ----------*/console.log("function expration ");/*----------------*/
