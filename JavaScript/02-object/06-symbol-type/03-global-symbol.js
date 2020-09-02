c95a66e1c9fbd7c10415d826e5bd7df819794dec
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-02 19:39:51 +0530

    global Symbol in .js file in javascript

--- JavaScript/02-object/06-symbol-type/03-global-symbol.js
+++ JavaScript/02-object/06-symbol-type/03-global-symbol.js
@@ -1,3 +1,34 @@
 // global Symbol
 
-/****** */ console.log(".........global symbol-------------------");
+/****** */ console.log("-------------01------------symbol.for------global symbol-------------------");
+
+
+let id = Symbol.for("id");    // if the Symbol did not exist
+
+let idAgain = Symbol.for("id");   // read it again ( maybe from another part of the code)
+
+console.log(id === idAgain);      // true
+
+
+// // ----while--
+//  let id = Symbol("id");
+//  let idd = Symbol("id");
+//  console.log(id == idd);    // false
+
+
+
+
+
+/** */ console.log("------------------02------- Symbol.keyFor----------");
+
+// get symbol by name
+ let sym1 = Symbol.for("name");
+ let sym2 = Symbol.for("id");
+
+ // get name by symbol
+
+ console.log( Symbol.keyFor(sym1));      //name
+ console.log(Symbol.keyFor(sym2));      // id 
+
+
+
