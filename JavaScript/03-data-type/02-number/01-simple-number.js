236ae44a349d13560352bc1daa077d638e3f7122
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-03 13:21:54 +0530

    simple number (type) of data type in javascript

---
+++ JavaScript/03-data-type/02-number/01-simple-number.js
@@ -0,0 +1,18 @@
+// number  
+/***** */  console.log(" -------------------01-----------more way to write a number---------------------------------");
+// one billion = 1000000000
+
+
+// 1e3 = 1 * 1000
+// 1.32e4 = 1.32 * 10000
+
+let billion = 1e9;     // 1 billion, literally : 1 and 9 zeros
+console.log(billion);   // 100000000
+
+
+/**   */console.log("--------------------------------");
+
+console.log(2e4);   // 20000    , 2 and ziros
+
+console.log(7.3e9);   // 7.3 billions  ( 7,300,000,000)
+
