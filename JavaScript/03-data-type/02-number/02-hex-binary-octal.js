1f19c8680d79c784f7b244bebabddf9b45e8e6ad
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-03 13:40:56 +0530

    syntex of hexadecimal, binary and octa number in javascript

---
+++ JavaScript/03-data-type/02-number/02-hex-binary-octal.js
@@ -0,0 +1,25 @@
+/** */  console.log("---------------01------Hexa desimal-------------------------------------");
+
+
+console.log(0xff);     // 255
+console.log(0xFF);        // 155
+
+
+
+
+/** */  console.log("---------------02------binary number-------------------------------------");
+
+console.log(0b11111111); //   255   binary number
+
+
+
+
+/** */  console.log("---------------03------octal number-------------------------------------");
+  console.log(0o377);       // 255 octal number
+
+
+  /************* */ console.log("-----------------------------");
+
+  let a = 0b11111111;
+  let b = 0o377;
+  console.log( a == b );    // true   ( 255 == 255)
