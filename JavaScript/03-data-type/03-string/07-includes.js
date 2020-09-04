806e09ca07479fa15802e04a7b5ccdd87c359744
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-04 13:18:43 +0530

    .includes function of string in javascript

---
+++ JavaScript/03-data-type/03-string/07-includes.js
@@ -0,0 +1,17 @@
+//  .include function
+// str.include(substr, pos)       returns true/false
+
+
+
+console.log("whild with id".includes("whild"));      // true
+console.log("whild with id".includes("hello"));      // false
+
+
+console.log("-------------------------------------------------");
+console.log("Widget".includes("id"));  // true
+console.log("Widget".includes("id",3));   // false   , start from 3rd position in no id
+
+
+
+console.log("-------------------------------------------------");
+
