fb61f7db070e6493ba3d748d0c664badcb7d006b
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-05 14:15:16 +0530

    neu Array() function of array in javascript

---
+++ JavaScript/03-data-type/04-array/06-new-array-function.js
@@ -0,0 +1,7 @@
+//  new Array()
+/** */ console.log("-----------------------new Array() --------------------");
+
+// let arr =new Array("Apple", "Pear", "etc");
+let arr = new Array(2);
+console.log(arr[0]);          // undefined
+console.log(arr.length);       // 2
