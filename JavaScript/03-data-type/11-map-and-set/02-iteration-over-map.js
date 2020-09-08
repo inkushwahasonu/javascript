816214dceab77f96b3017cf624d2f97833a88425
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-08 16:30:44 +0530

    iteration over map in javascript

--- JavaScript/03-data-type/11-map-and-set/02-iteration-over-map.js
+++ JavaScript/03-data-type/11-map-and-set/02-iteration-over-map.js
@@ -7,3 +7,25 @@ map.values() – returns an iterable for values,
 map.entries() – returns an iterable for entries [key, value], it’s used by default in for..of.
 
  */
+
+/**** */console.log("------------------------01----------Iteration over map----------------------");
+
+let recipeMap = new Map([
+  ['cucumber', 500],
+  ['tomatoes',350],
+  ['onion', 50]
+]);
+// iteration over key (vegetables)
+for(let vegetable of recipeMap.keys()){
+  console.log(vegetable);  // cucumber, tomatoes, onion
+}
+
+// iterate over values(amounts)
+for (let amount of recipeMap.values()){
+  console.log(amount);  // 500, 350, 50
+}
+
+// iterate over [key, values] entries
+for (let entry of recipeMap){
+  console.log(entry);
+}
