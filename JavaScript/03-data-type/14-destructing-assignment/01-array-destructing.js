ec706195cd678877553af85a686d175363406ff3
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-09 19:54:21 +0530

    transforming objects ( of key, values, entries) in javascript

--- JavaScript/03-data-type/14-destructing-assignment/01-array-destructing.js
+++
@@ -1,8 +0,0 @@
-// ignore elements using commas
-/*** */ console.log("-----------------------------------01------------ignore elements using commas----------------------------");
-
-
-
-// second & third element is not need  ( ignored by comma)
-let [firstName, , ,title] = ["Julius", "Caeser", "Consul", "of the Roman Republic"];
-console.log(title);   // Consul
