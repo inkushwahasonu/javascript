89420676ec6f2558e723d59bd0558748da31831a
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-14 18:37:10 +0530

    toJSON of JSON methods in javascript

---
+++ JavaScript/03-data-type/16-JSON/05-toJSON.js
@@ -0,0 +1,42 @@
+let room = {
+  number : 23
+};
+
+let meetup = {
+  title : "Conference",
+  date : new Date(Date.UTC(2017, 0, 1)),
+  room
+};
+
+console.log(JSON.stringify(meetup));
+
+/*
+{
+  "title":"Conference",
+  "date":"2017-01-01T00:00:00.000Z",
+  "room":{"number":23}
+}
+*/
+
+/***** */  console.log("---------------------------------------------------------");
+
+let room2 = {
+  number : 23,
+  toJSON() {
+    return this.number;
+  }
+};
+let meetup2 = {
+  title : "Conference",
+  room
+};
+
+console.log(JSON.stringify(room2)); // 23
+console.log(JSON.stringify(meetup2));
+
+/*
+  
+  23
+{"title":"Conference","room":{"number":23}}
+  
+*/
