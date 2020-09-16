be8c087e858f899d003ebb7f6c188f5ef42168f5
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-16 11:36:19 +0530

    recursive thinking (simplify the task and call self) of recursion and stack in javascaript

--- JavaScript/04-advance-working-with-function/01-recursion-and-stack/01-recursion-and-stack.js
+++ JavaScript/04-advance-working-with-function/01-recursion-and-stack/01-recursion-and-stack.js
@@ -19,4 +19,20 @@ function pow(x, n){
 console.log(pow(2, 3));      // 8
 
 
-// 
+// 2. recursive thinking : simplify the task and call self
+
+function pow2(x, n){
+  if( n== 1){
+    return x;
+
+  }
+  else{
+    return x * pow2(x, n -1);
+  }
+}
+console.log(pow2(2, 3));       //8
+
+/**
+*If n == 1, then everything is trivial. It is called the base of recursion, because it immediately produces the obvious result: pow(x, 1) equals x.
+*Otherwise, we can represent pow(x, n) as x * pow(x, n - 1). In maths, one would write xn = x * xn-1. This is called a recursive step: we transform the task into a simpler action (multiplication by x) and a simpler call of the same task (pow with lower n). Next steps simplify it further and further until n reaches 1.
+*/
