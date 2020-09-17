47fe0ce7f2947e01813ae937464bbdd4d9d20acb
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-17 18:43:07 +0530

    custom property of function object of advance working with function in javascript

---
+++ JavaScript/04-advance-working-with-function/06-function-object-NFE/03-custom-properties.js
@@ -0,0 +1,56 @@
+
+ // custom properties
+
+//  we can also add properties of our own
+// Here we add the counter property to track the total calls count :
+
+function sayHi(){
+  console.log("Hi");
+
+  // let's count hoe many times we run
+  sayHi.counter++;
+}
+sayHi.counter = 0; // initial value
+
+sayHi();   // Hi
+sayHi();   // Hi
+console.log(`Called ${sayHi.counter} times`);  // called 2 times
+
+
+
+
+
+
+
+/** */ console.log("-------------------------------------------");
+
+function makeCounter(){
+  // instead of :
+  // let count = 0
+  function counter(){
+    return counter.count++;
+  };
+  counter.count = 0;
+  return counter ;
+}
+
+let counter = makeCounter();
+console.log(counter());     // 0
+console.log(counter());    //1
+
+
+
+/** */ console.log("-------------------------------------------");
+
+function makeCounter2(){
+  function counter2(){
+    return counter2.count++;
+  };
+
+  counter2.count = 0;
+  return;
+}
+
+let counter5= makeCounter2();
+counter.count = 10;
+console.log(counter() ); // 10
