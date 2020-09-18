1cf0dfec4a9099b45227dd3cce0189c7f65bfc8d
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-18 19:12:52 +0530

    'transparent caching' of advance vorking with function in javascript

---
+++ JavaScript/04-advance-working-with-function/09-decorators-forwarding-call-apply/01-transparent-caching.js
@@ -0,0 +1,27 @@
+// Transparent caching
+
+function slow (x){
+  // there can be a heavy CPU- intensive job here
+  console.log(`Called with ${x}`);
+  return x;
+}
+
+function cachingDecorator(func){
+  let cache = new Map();
+  return function(x){
+    if(cache.has(x)){        // if there such key in cache
+      return cache.get(x);   // read the result from it
+    }
+    let result = func(x);   // otherwise call func
+    cache.set(x, result);   //and cache (remember) the result
+    return result;
+  };
+}
+
+slow = cachingDecorator(slow);
+
+console.log(slow(1));      // slow(1)  is cached
+console.log("Again : " + slow(1)); // the same
+
+console.log(slow(2));   // slow(2) is cached
+console.log("Again : " + slow(2));    // the same as the previous line
