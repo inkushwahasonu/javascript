3fa3e8120f6daad09f95ea60578bf74f79509928
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-18 19:56:22 +0530

    complete explanation of  'func.call' of advance working with function in javascript

--- JavaScript/04-advance-working-with-function/09-decorators-forwarding-call-apply/02-func-call.js
+++ JavaScript/04-advance-working-with-function/09-decorators-forwarding-call-apply/02-func-call.js
@@ -33,3 +33,59 @@ console.log(worker.slow(1));           // the original method works
 
 worker.slow = cachingDecorator(worker.slow);    // now make it caching
 /* console.log(worker.slow(2));  */              // Whoops! Error : Can not read property 'someMethod' of undefined
+
+
+
+
+
+/***** */ console.log("------------------------------------------------------------------");
+
+function sayHi(){
+  console.log(this.name);
+}
+let user2 = {name : "John"};
+let admin = {name :"Admin"};
+
+// use call to pass different objects as "this"
+sayHi.call(user2);    // John
+sayHi.call(admin);    // Admin
+
+
+/***** */ console.log("------------------------------------------------------------------");
+
+function say(phrase) {
+  console.log(this.name + ' : ' + phrase);
+}
+let user3 = {name : "John"};
+
+// user becomes this, and "Hello" Becomes the first argument
+say.call(user3, "hello");   // John : Hello
+
+
+
+
+/***** */ console.log("------------------------------------------------------------------");
+let worker2 = {
+  someMethod(){
+    return 1;
+  },
+  slow(x){
+    console.log("Called with " + x);
+    return x * this .someMethod();    
+  }
+};
+
+function cachingDecorator2(func){
+  let cache = new Map();
+  return function(x){
+    if(cache.has(x)){
+      return cache.get(x);
+    }
+    let result = func.call(this, x);        // "this" is passed correctly now
+    cache.set(x, result);
+    return result;
+  };
+}
+worker2.slow = cachingDecorator2(worker2.slow);   // now make it caching
+console.log(worker2.slow(2));    // works
+console.log(worker2.slow(2));     // works, doesn't call the original (cached)
