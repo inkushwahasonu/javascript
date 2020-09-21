80e14f65627f079f014aa34577d0304769e62204
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-21 11:30:25 +0530

    other built-in prototype ( function, date, array) of inheritance in javascript

---
+++ JavaScript/06-prototype-inheritance/03-native-prototype/02-other-built-in-prototype.js
@@ -0,0 +1,23 @@
+let arr = [1, 2, 3];
+
+// it inherits from Array.prototype
+console.log(arr.__proto__ === Array.prototype); // true
+
+// then from Object.prototype?
+console.log(arr.__proto__.__proto__ === Object.prototype);  // true
+
+// and null on the top.
+console.log(arr.__proto__.__proto__.__proto__);   // null
+
+
+
+/** */ console.log("-----------------------------------------------");
+let arr2 = [1,2,3]
+console.log(arr2);    // 1,2,3  <-- the result of Array.prototype.toString
+
+
+/*** */ console.log("----------------------------------------------");
+
+function f(){}
+  console.log(f.__proto__ == Function.prototype);   // true
+  console.log(f.__proto__.__proto__ == Object.prototype);  // true,  inherit from objects
