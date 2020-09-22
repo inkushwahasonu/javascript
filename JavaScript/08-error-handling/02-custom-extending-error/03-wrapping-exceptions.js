fd553993648711d141d6bd71b71cbfe56979e9a0
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-22 21:10:21 +0530

    wrapping-exception of extending error of error handling in javascript

---
+++ JavaScript/08-error-handling/02-custom-extending-error/03-wrapping-exceptions.js
@@ -0,0 +1,63 @@
+
+// wrapping exception
+
+
+class ReadError extends Error{
+  constructor(message, cause) {
+    super(message);
+    this.cause = cause;
+    this.name = 'ReadError';
+  }
+}
+
+class ValidationError extends Error {/*....*/}
+class PropertyRequiredError extends ValidationError {/*....*/}
+
+function validationUser(user){
+  if(!user.age){
+    throw new PropertyRequiredError("age");
+  }
+  if(!user.name) {
+    throw new PropertyRequiredError("name");
+  }
+}
+
+function readUser(json) {
+  let user;
+
+
+try {
+  user = JSON.parse(json);
+} catch (err){
+  if (err instanceof SyntaxError) {
+    throw new ReadError ("Syntax Error", err);
+  }else {
+    throw err;
+  }
+}
+
+
+try{
+  validationUser(user);
+}catch (err) {
+  if ( err instanceof ValidationError){
+    throw new ReadError("Validation Error", err);
+  }else {
+    throw err;
+  }
+}
+}
+
+
+try {
+  readUser('{bad json');
+}
+catch (e) {
+  if (e instanceof ReadError){
+    console.log(e);
+    // Origin error: SyntaxError : Unexpected token b i JSON at position 1
+    console.log("Original error : " + e.cause);
+  } else {
+    throw e;
+  }
+}
