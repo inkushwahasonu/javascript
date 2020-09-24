9d98f5aeb5f4e8bc2bf843e190704d14974760cf
inkushwahasonu <inkushwahasonu@gmail.com>
2020-09-24 16:25:36 +0530

    promisification in javascript

---
+++ JavaScript/09-promises-async-await/06-promisification/promisification.js
@@ -0,0 +1,12 @@
+
+// promisification
+
+function loadScript(src, callback){
+  let script = document.createElement('script');
+  script.src =src;
+
+  script.onload = () => callback (null, script);
+  script.onerror = () => callback (new Error(`Script load error for ${src}`));
+
+  document.head.append(script);
+}
