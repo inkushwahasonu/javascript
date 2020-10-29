96ae5c0fb239096e16ec9fe06063301635dc7d43
inkushwahasonu <inkushwahasonu@gmail.com>
2020-10-29 18:48:26 +0530

    add some functionality in input vlaue in form validation in javascript

--- JavaScript/04-form-validation/03/script.js
+++ JavaScript/04-form-validation/03/script.js
@@ -29,7 +29,7 @@ function signup(event) {
   if (username.value === "") {
     username.classList.add("invalid");
     usernameErrorIcon.classList.add("errorIcon");
-    usernameErrorMsg.classList.remove("usernameInstruction");
+    usernameErrorMsg.classList.remove("instruction");
     usernameErrorMsg.classList.add("error");
     usernameErrorMsg.innerHTML = "please fill user-name, it can't be empty!";
     return false;
@@ -42,9 +42,72 @@ function signup(event) {
 
   // ---------- email validation---------------------
 
+  if(email.value === ""){
+    email.classList.add("invalid");
+    emailErrorIcon.classList.add("errorIcon");
+    emailErrorMsg.classList.remove("instruction");
+    emailErrorMsg.classList.add("error");
+    emailErrorMsg.innerHTML = "please fill email field, it can't be empty !"
+    return false;
+  }else{
+    email.classList.remove("invalid");
+    emailErrorIcon.classList.remove("errorIcon");
+    emailSuccessIcon.classList.add("successIcon");
+    emailErrorMsg.innerHTML = "";
+  }
+
   // ---------- phone validation---------------------
+
+  if(phone.value === ""){
+    phone.classList.add("invalid");
+    phoneErrorIcon.classList.add("errorIcon");
+    phoneErrorMsg.classList.remove("instruction");
+    phoneErrorMsg.classList.add("error");
+    phoneErrorMsg.innerHTML = "please fill copassword field, it can't be empty !"
+    return false;
+  }else{
+    phone.classList.remove("invalid");
+    phoneErrorIcon.classList.remove("errorIcon");
+    phoneSuccessIcon.classList.add("successIcon");
+    phoneErrorMsg.innerHTML = "";
+  }
+
+
 
   // ---------- password validation---------------------
 
+  if(password.value === ""){
+    password.classList.add("invalid");
+    passwordErrorIcon.classList.add("errorIcon");
+    passwordErrorMsg.classList.remove("instruction");
+    passwordErrorMsg.classList.add("error");
+    passwordErrorMsg.innerHTML = "please fill password field, it can't be empty !"
+    return false;
+  }else{
+    password.classList.remove("invalid");
+    passwordErrorIcon.classList.remove("errorIcon");
+    passwordSuccessIcon.classList.add("successIcon");
+    passwordErrorMsg.innerHTML = "";
+  }
+
   // ---------- confirm password validation---------------------
+
+  if(cpassword.value === ""){
+    cpassword.classList.add("invalid");
+    cpasswordErrorIcon.classList.add("errorIcon");
+    cpasswordErrorMsg.classList.remove("instruction");
+    cpasswordErrorMsg.classList.add("error");
+    cpasswordErrorMsg.innerHTML = "please fill password field, it can't be empty !"
+  }else if(password.value === cpassword.value){
+      cpassword.classList.remove("invalid");
+      cpasswordErrorIcon.classList.remove("errorIcon");
+      cpasswordSuccessIcon.classList.add("successIcon");
+      cpasswordErrorMsg.innerHTML = "";
+  }else {
+    cpassword.classList.add("invalid");
+    cpasswordErrorIcon.classList.add("errorIcon");
+    cpasswordErrorMsg.classList.remove("instruction");
+    cpasswordErrorMsg.classList.add("error");
+    cpasswordErrorMsg.innerHTML = "password invalid!"
+  }
 }
