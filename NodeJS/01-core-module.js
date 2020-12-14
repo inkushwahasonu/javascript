cd8077f5c721bf66507b0a88a24b523457e110ec
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-14 18:08:29 +0530

    read file data in string formate

--- NodeJS/01-core-module.js
+++ NodeJS/01-core-module.js
@@ -13,13 +13,18 @@ fsys.appendFileSync('read.txt', '\nadd some message of file system  and this is 
 
 // read the data of previous file (read.txt)
 let buf_data = fsys.readFileSync('read.txt');
-console.log(buf_data);
+// console.log(buf_data);                      to print file-data in buffer..
+// <Buffer 6f 76 65 72 2d 72 69 64 65 20 6f 66 20 66 69 6c 65 20 73 79 73 74 65 6d 20 74 68 69 73 20 69 73 20 74 65 73 74 69 6e 67 20 6f 66 20 63 6f 72 65 20 6d ... >
 
 /**
- * <Buffer 6f 76 65 72 2d 72 69 64 65 20 6f 66 20 66 69 6c 65 20 73 79 73 74 65 6d 20 74 68 69 73 20 69 73 20 74 65 73 74 69 6e 67 20 6f 66 20 63 6f 72 65 20 6d ... >
  * 
  * Node-js includes a aditional datatype called buffer.
  * (not available in browser's JavaScript)
  * buffer is mainly used to store binary data.
  * while reading from the file or receiving packets over the network .
  */
+
+
+// print messages of file in string formate
+ org_data = buf_data.toString();
+console.log(org_data);
