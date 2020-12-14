0297d9d4405055a54a06fb9cf067db81e01203d2
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-14 17:06:55 +0530

    starting of core module with file system in nodeJS

--- NodeJS/01-core-module.js
+++ NodeJS/01-core-module.js
@@ -1,2 +1,3 @@
 const fs = require("fs");
-fs.writeFileSync("read.txt","this is core module");
+fs.writeFileSync("read.txt","this is core module with file system.");
+fs.writeFileSync("read.txt","over-ride of file syster this is testing of core module with file system");
