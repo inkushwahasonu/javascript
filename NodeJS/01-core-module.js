850367f9332a6fa4086b6004f05a4c9ceace0d6d
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-14 17:59:57 +0530

    read file in buffer

--- NodeJS/01-core-module.js
+++ NodeJS/01-core-module.js
@@ -10,3 +10,16 @@ fsys.writeFileSync("read.txt","over-ride of file system this is testing of core 
 
 // add some another messages in privious file (read.txt)
 fsys.appendFileSync('read.txt', '\nadd some message of file system  and this is a testing of core module with file system.');
+
+// read the data of previous file (read.txt)
+let buf_data = fsys.readFileSync('read.txt');
+console.log(buf_data);
+
+/**
+ * <Buffer 6f 76 65 72 2d 72 69 64 65 20 6f 66 20 66 69 6c 65 20 73 79 73 74 65 6d 20 74 68 69 73 20 69 73 20 74 65 73 74 69 6e 67 20 6f 66 20 63 6f 72 65 20 6d ... >
+ * 
+ * Node-js includes a aditional datatype called buffer.
+ * (not available in browser's JavaScript)
+ * buffer is mainly used to store binary data.
+ * while reading from the file or receiving packets over the network .
+ */
