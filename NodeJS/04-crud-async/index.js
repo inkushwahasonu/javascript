4c2cdb7970d30ee34fee8a55b899a0cdd94abb3a
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-15 18:48:32 +0530

    append/add some another data in file in asynchronous file system of core module in node js

--- NodeJS/04-crud-async/index.js
+++ NodeJS/04-crud-async/index.js
@@ -1,6 +1,5 @@
+const fs = require("fs");
 
- const fs = require("fs");
- 
 /*
 
  // create a folder
@@ -11,9 +10,22 @@
 
  */
 
+// create a file in any fiolder
+fs.writeFile(
+  "sonu/bio.txt",
+  "This is CRUD operation in asynchronous file system in core module in nodeJS",
+  (err) => {
+    console.log("file is created and first message added.");
+    console.log(`error : ${err}.`);
+  }
+);
 
- // create a file in any fiolder
- fs.writeFile("sonu/bio.txt","This is CRUD operation in asynchronous file system in core module in nodeJS",(err) => {
-   console.log("file is created and first message added.");
-   console.log(`error : ${err}.`);
- })
+// add some another messages in file without overriding
+fs.appendFile(
+  "sonu/bio.txt",
+  "\nappend some another messages in CRUD operation in asynchronous file system in core module in node js.",
+  (err) => {
+    console.log("data added.");
+    console.log(`error ${err}`);
+  }
+);
