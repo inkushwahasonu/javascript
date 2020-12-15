7e716521a413ab3d115cac5c66a37ef8fdb3a44c
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-15 16:04:48 +0530

    append/add more data in  Asynchronous file system in core module in node js

--- NodeJS/03-fs-async/index.js
+++ NodeJS/03-fs-async/index.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 
 
+// create a file and write some message  in that file
 fs.writeFile("read.txt","This is a asynchronous file system in core module in node js.", (err) => {
   console.log("file is created");
+  console.log(err);
 })
+
+// add some more data in that file
+ fs.appendFile("read.txt","\nappend/add some more data in asynchronous file system", (err) => {
+ console.log("data added..");
+ console.log(err);
+ })
