d85dce5e90c03417bd5e20eb8fe812f7dc6c50e3
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-15 18:42:41 +0530

    create a file in any folder and write some first message in CRUD operation in asynchronous file system of core module in nodeJS

--- NodeJS/04-crud-async/index.js
+++ NodeJS/04-crud-async/index.js
@@ -1,9 +1,19 @@
 
  const fs = require("fs");
  
+/*
 
  // create a folder
  fs.mkdir("sonu",(err) => {
    console.log("folder created");
    console.log(` error : ${err}. `);
+ })
+
+ */
+
+
+ // create a file in any fiolder
+ fs.writeFile("sonu/bio.txt","This is CRUD operation in asynchronous file system in core module in nodeJS",(err) => {
+   console.log("file is created and first message added.");
+   console.log(`error : ${err}.`);
  })
