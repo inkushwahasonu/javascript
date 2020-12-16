64e59669ded32e039db21717eb8fc7686f408794
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-16 17:57:35 +0530

    find extension of present file by PATH modyle asynchronously in nodeJS

--- NodeJS/06-path-module/path.js
+++ NodeJS/06-path-module/path.js
@@ -4,3 +4,6 @@ const path = require("path");
 // print directory name / directory path of this file
 console.log(path.dirname("/home/sonu/practices/javascript/NodeJS/06-path-module/path.js"));
 
+
+// print extension of this file
+console.log(path.extname("/home/sonu/practices/javascript/NodeJS/06-path-module/path.js"));
