7f066aaeb5042ee4b624eeaa4f02c4599d8cf824
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-16 17:56:40 +0530

    find directory/folder path of present file by PATH modyle asynchronously in nodeJS

---
+++ NodeJS/06-path-module/path.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+
+// print directory name / directory path of this file
+console.log(path.dirname("/home/sonu/practices/javascript/NodeJS/06-path-module/path.js"));
+
