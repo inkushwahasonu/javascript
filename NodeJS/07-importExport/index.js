7192a994b1e4c8ac9c1d9c7d52a5e25005f4a1e4
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-16 19:11:51 +0530

    in a simple way, create own module for multiple function accessing in node js

--- NodeJS/07-importExport/index.js
+++ NodeJS/07-importExport/index.js
@@ -1,8 +1,6 @@
-
 // // access for single function of any module
 // const name = require('./operator');
 // console.log(name);
-
 
 // access for multiple function of any module
 /*
@@ -13,6 +11,7 @@ console.log(oper.sub(10, 6));
 
 // or
 
-const {add, sub} = require('./operator')
+const { add, sub, mult } = require("./operator");
 console.log(add(4, 5));
 console.log(sub(9, 4));
+console.log(mult(3, 4));
--- NodeJS/07-importExport/operator.js
+++ NodeJS/07-importExport/operator.js
@@ -1,15 +1,21 @@
-
 // //for single function
 // const name = "sonu";
 // module.exports = name;
 
 const add = (a, b) => {
-  return a + b ;
+  return a + b;
 };
 const sub = (c, d) => {
-  return c - d ;
+  return c - d;
+};
+const mult = (e, f) => {
+  return e * f;
 };
 
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.mult = mult;
 
-module.exports.add = add;
-module.exports.sub = sub;
+// or
+
+module.exports = { add, sub, mult };
