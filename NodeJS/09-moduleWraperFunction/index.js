aa56f77cd37430e1cc54e5a0337f521e2cc33d73
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-17 18:40:13 +0530

    wraper function and two another properties in node js

---
+++ NodeJS/09-moduleWraperFunction/index.js
@@ -0,0 +1,10 @@
+// module wraper function
+
+(function(exports, require, module, __filename, __dirname){
+  const name = 'sonu';
+console.log(name);
+})();
+
+
+console.log(__dirname);
+console.log(__filename);
