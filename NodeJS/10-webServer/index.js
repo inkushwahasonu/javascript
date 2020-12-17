5fcd3528532bf70318d554b4667fd61c748f249f
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-17 19:16:17 +0530

    create web server in node js

---
+++ NodeJS/10-webServer/index.js
@@ -0,0 +1,12 @@
+
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from inkushwahasonu");
+});
+
+server.listen(7000,"127.0.0.1",() => {
+  console.log("listening to the port no 7000");
+});
