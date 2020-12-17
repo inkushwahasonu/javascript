30267f43653b9be7f8cb54028a5a6369f97868be
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-17 19:42:14 +0530

    routing pages / handled http requests in onde js

--- NodeJS/10-webServer/index.js
+++ NodeJS/10-webServer/index.js
@@ -4,7 +4,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from inkushwahasonu");
+  // console.log(req.url);
+  if(req.url == "/"){
+    res.end("Hello in HOME inkushwahasonu");
+  }else if(req.url == "/about"){
+    res.end("Hello in ABOUTUS of inkushwahasonu");
+  }else if(req.url == "/contact"){
+    res.end("CONTECT US...");
+  }else {
+    res.writeHead(404,{"Content-type" : "text/html"});   
+    res.end("<h1>404 : error , page does not exist...</h1>");
+  }
+ 
 });
 
 server.listen(7000,"127.0.0.1",() => {
