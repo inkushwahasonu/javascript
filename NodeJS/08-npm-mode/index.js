e524fa173beabeb609009988b8fbd1942fd33355
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-17 16:12:56 +0530

    use of validator package of npm in node js

--- NodeJS/08-npm-mode/index.js
+++ NodeJS/08-npm-mode/index.js
@@ -1,2 +1,9 @@
 const chalk = require("chalk");
+const validator = require("validator");
 console.log(chalk.blue.underline.inverse("Hello world"));
+
+const res = validator.isEmail("inkushwahasonu@gmail.com");
+console.log(res);
+
+// or 
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
