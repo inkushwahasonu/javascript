064fb10e59642f7e5bb370dc89a164b88043770b
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-18 10:45:57 +0530

    another way to  access a pirticular term of json/api file in node js

--- NodeJS/12-api/index.js
+++ NodeJS/12-api/index.js
@@ -2,22 +2,21 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  // console.log(req.url);
+
+  const data = fs.readFileSync(`${__dirname}/api.json`, "UTF-8");
+  const objData = JSON.parse(data);
+
   if (req.url == "/") {
     res.end("HOME page...");
   } else if (req.url == "/about") {
     res.end("ABOUT us...");
   } else if (req.url == "/contact") {
     res.end("CONTACT us....");
-
-  } else if (req.url == "/userApi") {               // access api data
-    fs.readFile(`${__dirname}/api.json`, "UTF-8", (err, data) => {
-      console.log(data);
-      // res.end(data);
-      const objData = JSON.parse(data);
-      res.end(objData[0].name);
-    });
-    // res.end("user API....");
+  } else if (req.url == "/userApi") {
+    // access api data
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(objData[0].name);
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("<h1> 404 : ERROR <br> page does not exist....</h1>");
