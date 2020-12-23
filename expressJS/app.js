6584abfea41e992001e710749d95f7296604520c
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-23 08:23:40 +0530

    print hello world of home page in express JS

---
+++ expressJS/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world from inkushwahasonu in express JS....");
+});
+
+app.listen(8000, () => {
+  console.log("listing the port at 8000..");
+});
