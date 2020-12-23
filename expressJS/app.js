45fbf760a261fe866b317ce7d1e705f38270fc23
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-23 08:28:40 +0530

    create routing for another pages in express js

--- expressJS/app.js
+++ expressJS/app.js
@@ -4,6 +4,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello world from inkushwahasonu in express JS....");
 });
+app.get("/about", (req,res) =>{
+  res.send("Hello from the about page ......");
+});
+
 
 app.listen(8000, () => {
   console.log("listing the port at 8000..");
