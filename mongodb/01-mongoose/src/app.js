3ec53930e2c89f3278ec99c46898a37a87ac1781
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-02 11:51:54 +0530

    create collection for mongodb

--- mongodb/01-mongoose/src/app.js
+++ mongodb/01-mongoose/src/app.js
@@ -1,6 +1,37 @@
 const mongoose = require("mongoose");
 
-//connection creation 
-mongoose.connect("mongodb://localhost:27017/sonudata", { useUnifiedTopology: true, useNewUrlParser: true  } )
-.then(() => console.log("connection successfull"))
-.catch((err) => console.log(err));
+//connection creation
+mongoose
+  .connect("mongodb://localhost:27017/sonudata", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("connection successfull"))
+  .catch((err) => console.log(err));
+
+// schema
+// A mongoose schema defines the structure of the document,
+// default values, validation, etc.,
+
+const playlistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: String,
+  author: String,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Amongoose model is a wrapper on the Mongoose schema.
+// A Mongoose schema defines the structure of the document,
+// default values, validator,etc., whereas a Mongoose model
+// Provides an interface to the database for creating,
+// Querying, updating, deleting records,etc.
+
+// collection creationst",playlistSchema);
+const Playlist = new mongoose.model("Playlist", playlistSchema);
