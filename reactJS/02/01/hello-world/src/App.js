d9544523b97f1c84020b003c5fe25db775f5173a
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-17 12:30:47 +0530

    functional component in react js

--- reactJS/02/01/hello-world/src/App.js
+++ reactJS/02/01/hello-world/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+// import Greet from './components/Greet';
+import { Greet } from './components/Greet';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Greet />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ reactJS/02/01/hello-world/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Greet = () => {       // stateless functional component
+  return ( 
+    <h1>Hello inkush</h1>
+   );
+}
+
+// export default Greet;
