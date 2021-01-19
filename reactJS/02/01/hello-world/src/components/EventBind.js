7fa3ba55b545743f9c7d458ae148a75aad623b34
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-19 13:59:08 +0530

    EventBind in reactJS

--- reactJS/02/01/hello-world/src/components/EventBind.js
+++ reactJS/02/01/hello-world/src/components/EventBind.js
@@ -1,34 +1,40 @@
+// chenge your message after clicking the button
 
+import React, { Component } from "react";
+class EventBind extends Component {
+  constructor(props) {
+    super(props);
 
-import React, {Component} from 'react';
-class EventBind extends Component{
-  constructor(props){
-    super(props)
-
-    this.state ={
-      message: 'Hello Event Bind'
-    }
+    this.state = {
+      message: "Hello Event Bind",
+    };
+    // this.clickHandler = this.clickHandler.bind(this);  // cunstructor approch
   }
 
-  clickHandler(){
-    // this.setState({
-    //   message2: 'GoodBye!'
-    // })
-    
-  }
+  // clickHandler(){
+  //   this.setState({
+  //     message: 'GoodBye!'
+  //   })
+  //   // console.log(this);
+  // }
 
-  render(){
-    return(
+  clickHandler = () => {
+    this.setState({
+      message: "GoodBye!",
+    });
+  };
+
+  render() {
+    return (
       <div>
-        <hr/>
+        <hr />
         <p>EventBind</p>
         <h1>{this.state.message}</h1>
-        <button onClick={this.clickHandler}>click here</button>
-        <h3>{this.state.message2}</h3>
+        {/* <button onClick={this.clickHandler.bind(this)}>click here</button> */}
+        <button onClick={() => this.clickHandler()}>Click here</button>   {/*for constructor approch*/}
+        <button onClick={this.clickHandler}>Click Here</button>      {/*for arrow function approch*/}
       </div>
-    )
+    );
   }
 }
 export default EventBind;
-
-
