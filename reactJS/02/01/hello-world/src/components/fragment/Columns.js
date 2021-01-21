898910e20b44854f8d81c73ba24cd6c66bfc250b
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-21 08:43:24 +0530

    <React.Fragment> in table in react JS

---
+++ reactJS/02/01/hello-world/src/components/fragment/Columns.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Columns() {
+  return (
+    <React.Fragment>
+      <td>Name</td>
+      <td>Sourav</td>
+    </React.Fragment>
+  );
+}
+export default Columns;
