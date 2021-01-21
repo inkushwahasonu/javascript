0b2f8ccef8be222d4550149efa6ab859c9c8d30a
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-21 19:06:42 +0530

    error handling phage method in react JS

--- reactJS/02/01/hello-world/src/App.js
+++ reactJS/02/01/hello-world/src/App.js
@@ -9,20 +9,22 @@ import State from "./components/State";
 import Counter from "./components/Counter";
 import Props2 from "./components/Props2";
 import FunctionClick from "./components/FunctionClick";
-import EventBind from './components/EventBind';
-import ParentComponent from './components/parent/ParentComponent';
-import ConditionalRendring from './components/ConditionalRendring';
-import {NameList} from './components/map/NameList';
-import StyleSheet from './components/css/StyleSheet';
-import Inline from './components/css/Inline';
-import Form from './components/Form';
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/parent/ParentComponent";
+import ConditionalRendring from "./components/ConditionalRendring";
+import { NameList } from "./components/map/NameList";
+import StyleSheet from "./components/css/StyleSheet";
+import Inline from "./components/css/Inline";
+import Form from "./components/Form";
 // import LifecycleA from './components/mounting-lifecycle/LifecycleA';
-import FragmentDemo from './components/fragment/FragmentDemo';
-import Table from './components/fragment/Table';
+import FragmentDemo from "./components/fragment/FragmentDemo";
+import Table from "./components/fragment/Table";
 // import ParentComp from './components/PureComponent/ParentComp';
-import RefsDemo from './components/Refs/RefsDemo';
-import FRParentInput from './components/Refs/ForwardingRefs/FRParentInput';
-import PortalDemo from './components/PortalDemo';
+import RefsDemo from "./components/Refs/RefsDemo";
+import FRParentInput from "./components/Refs/ForwardingRefs/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/errorMethod/Hero";
+import ErrorBoundary from "./components/errorMethod/ErrorBoundary";
 
 class App extends Component {
   render() {
@@ -37,12 +39,12 @@ class App extends Component {
         <Counter />
         <Props2 name="Amit Props" heroName="arsad" />
         <Props2 name="Ankit Props" heroName="carli" />
-        <FunctionClick/>
-        <EventBind/>
-        <ParentComponent/>
+        <FunctionClick />
+        <EventBind />
+        <ParentComponent />
         <ConditionalRendring />
         <NameList />
-        <StyleSheet primary={true}/>
+        <StyleSheet primary={true} />
         <Inline />
         <Form />
         {/* <LifecycleA /> */}
@@ -52,6 +54,15 @@ class App extends Component {
         <RefsDemo />
         <FRParentInput />
         <PortalDemo />
+        <ErrorBoundary>
+          <Hero heroName="Batman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Superman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Joker" />
+        </ErrorBoundary>
       </div>
     );
   }
--- reactJS/02/01/hello-world/src/components/PortalDemo.js
+++ reactJS/02/01/hello-world/src/components/PortalDemo.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 
 const PortalDemo = () => {
   return ReactDOM.createPortal(
-    <h1>Portals Demo</h1>,
+    <div>
+      <hr/><h1>Portals Demo</h1>
+    </div>,
     document.getElementById("portal-root") // access from public/index.html
   );
 };
