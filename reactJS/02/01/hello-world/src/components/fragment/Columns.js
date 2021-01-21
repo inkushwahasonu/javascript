cdebc809937138f20cf9d46632bbc9bcd9669600
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-21 09:23:03 +0530

    key attribute in fragment in React js

--- reactJS/02/01/hello-world/src/components/fragment/Columns.js
+++ reactJS/02/01/hello-world/src/components/fragment/Columns.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 function Columns() {
+  const items =[];
   return (
     <React.Fragment>
+      {
+        items.map( item => (
+          <React.Fragment key ={item.id}>
+            <h1>Title</h1>
+            <p>{item.title}</p>
+          </React.Fragment>
+        ))
+      }
       <td>Name</td>
       <td>Sourav</td>
     </React.Fragment>
