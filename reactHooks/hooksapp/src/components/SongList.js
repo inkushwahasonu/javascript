c26a8f545e3761f6491513f387347ba0b94d94b0
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-26 18:30:32 +0530

    use of useState() method and install uuid for create unique id for object in React hooks

---
+++ reactHooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import uuid from 'uuid';     // install package for create defrent id (npm install uuid)
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gosip", id: 2 },
+    { title: "this wild darkness", id: 3 },
+  ]);
+  const addSong = () => {
+    setSongs([...songs, { title: "new song", id: uuid() }]);
+  };
+
+  return (
+    <div className="Song-list">
+      <ul>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </ul>
+      <button onClick={addSong}>Add a song</button>
+    </div>
+  );
+};
+
+export default SongList;
