84ddff935468cfb0aaa207cd22f19db7a42889b8
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-26 14:55:04 +0530

    click event  (change the state by clicking event ) in React hooks

--- reactHooks/contextapp/src/components/BookList.js
+++ reactHooks/contextapp/src/components/BookList.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 class BookList extends Component {
- static contextType = ThemeContext;
+  static contextType = ThemeContext;
 
   render() {
     const { isLightTheme, light, dark } = this.context;
     const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list" style={{color: theme.syntax, background:theme.bg}}>
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
         <ul>
-          <li style = {{background: theme.ui}}>the way of kings</li>
-          <li style = {{background: theme.ui}}>the name of the wind</li>
-          <li style = {{background: theme.ui}}>the final emire</li>
+          <li style={{ background: theme.ui }}>the way of kings</li>
+          <li style={{ background: theme.ui }}>the name of the wind</li>
+          <li style={{ background: theme.ui }}>the final emire</li>
         </ul>
       </div>
     );
--- reactHooks/contextapp/src/components/Navbar.js
+++ reactHooks/contextapp/src/components/Navbar.js
@@ -4,23 +4,24 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends Component {
   render() {
     console.log(this.context);
-    
-    
+
     return (
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } =context;
-        const theme = isLightTheme ? light : dark ;
-        return(
-          <nav style ={{background: theme.ui, color: theme.syntax}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-        )
-      }}</ThemeContext.Consumer>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
