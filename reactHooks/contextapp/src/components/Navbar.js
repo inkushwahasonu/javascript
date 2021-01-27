69382764a8ea189967ff10ba3ea7d6b0b0f86c1a
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-27 13:56:19 +0530

    convert class component into functional component  in React hooks

--- reactHooks/contextapp/src/components/Navbar.js
+++ reactHooks/contextapp/src/components/Navbar.js
@@ -1,40 +1,24 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class Navbar extends Component {
-  render() {
-    console.log(this.context);
-
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { isLightTheme, light, dark } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                  <h1>Context App</h1>
-                  <div onClick = {toggleAuth}>
-                    {isAuthenticated
-                      ? 'you are logged in'
-                      : 'you are Logged out'}
-                  </div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const Navbar = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? "you are logged in" : "you are Logged out"}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
