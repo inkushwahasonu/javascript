a01f0548b6e82a16390307d9829c267727f80055
inkushwahasonu <inkushwahasonu@gmail.com>
2021-01-27 13:37:49 +0530

    useContext() inside of functional component

--- reactHooks/contextapp/src/components/BookList.js
+++ reactHooks/contextapp/src/components/BookList.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 // import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
-class BookList extends Component {
-  static contextType = ThemeContext;
-  // static contextType = AuthContext;
 
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final emire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final emire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
